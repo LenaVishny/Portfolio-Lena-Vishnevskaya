8461f86c3db9df19a62b21715e9013fd2ba4010e
Lena <lenavishny3108@gmail.com>
2022-04-30 14:28:49 +0300

    background

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.onload = function() {
     let preloader = document.getElementById('preloader');
     preloader.classList.add('hide-preloader');
     setInterval(function() {
-          preloader.classList.add('preloader-hidden');
+        preloader.classList.add('preloader-hidden');
     }, 2990);
 }
 
